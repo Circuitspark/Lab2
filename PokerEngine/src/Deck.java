ae2468b47b16164af8a9f0fb549670f67456722a
Circuit <Circuit@10.0.0.5>
2015-03-03 13:32:03 -0500

    reup

--- PokerEngine/src/Deck.java
+++ PokerEngine/src/Deck.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
+
+	private ArrayList<Card> deck;
+
+	public Deck() {
+		
+		this.deck = new ArrayList<Card>();
+		for (int c = 0; c < 12; c++) {
+
+			Rank value = Rank.value()[c];
+
+			for (int s = 0; s < 4; s++) {
+
+				Card card = new Card(value, Suit.values()[s]);
+				this.deck.add(card);
+			}
+		}
+
+		Collections.shuffle(deck);
+
+	}
 
 }
